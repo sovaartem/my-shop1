4a38420d36958b82cb33e7a3b232ae6bb16f5071
sovaartem <sovaartem20092009@gmail.com>
2025-12-15 11:34:33 +0200

    Poduct.list

---
+++ my-shop/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app" >
+    <ProductCard  
+    name="Українська вишинка"
+    description="Стильна вишиванка з традиційними орнаментами"
+    price="1500"
+    image="https://upload.wikimedia.org/wikipedia/commons/7/75/Folklore_of_Sanok%2C_2010_1.JPG"  
+    />
+  </div>
+</template>
+
+<script>
+  import ProductCard from './components/ProductCard.vue';
+
+  export default {
+    components: {
+      ProductCard
+    }
+  }
+</script>
