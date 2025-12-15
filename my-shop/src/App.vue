c8e6bf7e9d32d68c43e31ec015dd628af10e7db3
sovaartem <sovaartem20092009@gmail.com>
2025-12-15 11:50:00 +0200

    LIst

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -6,15 +6,23 @@
     price="1500"
     image="https://upload.wikimedia.org/wikipedia/commons/7/75/Folklore_of_Sanok%2C_2010_1.JPG"  
     />
+    <ProductCard1
+    name="Футболка Chrome Hearts"
+    description="Стильна, брендова футболка "
+    price="700"
+    image="https://content1.rozetka.com.ua/goods/images/big/588729815.jpg"
+    />
   </div>
 </template>
 
 <script>
   import ProductCard from './components/ProductCard.vue';
+  import ProductCard1 from './components/ProductCard1.vue';
 
   export default {
     components: {
-      ProductCard
+      ProductCard,
+      ProductCard1
     }
   }
 </script>
